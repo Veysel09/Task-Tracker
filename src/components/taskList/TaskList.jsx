95619324c89fc77e9c888c8b24533d3d0cb137ae
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-23 20:30:20 +0300

    finish

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,6 +1,17 @@
+import axios from "axios";
 import { RiDeleteBack2Fill } from "react-icons/ri";
 
 const TaskList = ({ task, getTask }) => {
+
+
+  const deleteTask = async (id) => {
+    const url = "https://6351820edfe45bbd55c21b4d.mockapi.io/api/tasks";
+    try {
+      await axios.delete(`${url}/${id}`);
+    } catch (error) {}
+    getTask();
+  };
+
   return (
     <div>
       {task.map((item) => {
@@ -21,7 +32,10 @@ const TaskList = ({ task, getTask }) => {
                   marginRight: "20px",
                   fontSize: "2rem",
                   boxShadow: "2px 2px 2px #ECAB9E",
+                  
                 }}
+                onClick={() => deleteTask(id)}
+
               />
             </div>
           </div>
